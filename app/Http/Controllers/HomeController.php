1e820418c24376a6ddf53a68777a90fe2148f2b6
Mohamed Hussein <105607296+MohamedHussein43@users.noreply.github.com>
2023-11-27 17:56:35 +0200

    creating admin login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
+        return view("Home");
+    }
+    
+    public function redirects(){
+        $usertype = Auth::user()->usertype;
+        if ($usertype == '1') {
+            return view("admin.adminhome");
+        }
+        else{
+            return view("Home");
+        }
+
         return view("Home");
     }
 }
